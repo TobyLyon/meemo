1e86772defdc1d9ce384ba3685d94bc5914b5b77
Toby HMG <toby.hmg@gmail.com>
2025-11-06 10:21:45 -0800

    revert(token): set TOKEN_ADDRESS back to 'coming soon'

--- config/constants.ts
+++ config/constants.ts
@@ -1,5 +1,5 @@
 // Central place to update the live token address.
 // Replace the value below when you go live and commit.
-export const TOKEN_ADDRESS: string = "CzfDwWoTVY4pidbsTVmT61pKdSb1Ym2kQra93oBLpump";
+export const TOKEN_ADDRESS: string = "coming soon";
 
 
