519cd097568f6348f4fe0fe205d1ac3dd09a3f93
Toby HMG <toby.hmg@gmail.com>
2025-12-17 08:44:22 -0800

    Update token address

--- config/constants.ts
+++ config/constants.ts
@@ -1,6 +1,6 @@
 // Central place to update the live token address.
 // Replace the value below when you go live and commit.
-export const TOKEN_ADDRESS: string = "2AzmDsxQhcEsd7oLgJEn1Qb2SV4RvZ2v3SggqXuypump";
+export const TOKEN_ADDRESS: string = "4o3mN9wXFrzoojpXPxYxgTcioqr3Ca1kcLc48juNpump";
 
 export const TELEGRAM_URL: string = "https://t.me/meemosportal";
 
