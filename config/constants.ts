0c16583af7f604d1d5349a3cf58110235b889b42
Toby HMG <toby.hmg@gmail.com>
2025-12-16 18:23:53 -0800

    feat: set live TOKEN_ADDRESS and add Section 2 social icons

--- config/constants.ts
+++ config/constants.ts
@@ -1,5 +1,5 @@
 // Central place to update the live token address.
 // Replace the value below when you go live and commit.
-export const TOKEN_ADDRESS: string = "coming soon";
+export const TOKEN_ADDRESS: string = "2AzmDsxQhcEsd7oLgJEn1Qb2SV4RvZ2v3SggqXuypump";
 
 
